13c5898baed5b6ca4fc8b52d2131c9105939d598
mamat <mamatniroula1@gmail.com>
2020-05-05 17:09:35 -0500

    oauth server added

--- src/main/java/com/sunil/demo/OauthServerApplication.java
+++ src/main/java/com/sunil/demo/OauthServerApplication.java
@@ -2,14 +2,12 @@ package com.sunil.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
-@EnableOAuth2Sso
-public class OauthwithspringbootApplication {
+public class OauthServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OauthwithspringbootApplication.class, args);
+		SpringApplication.run(OauthServerApplication.class, args);
 	}
 
 }
